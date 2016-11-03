4829721000d3c0d4fd064820c44a69d454717e96
wrongu <rlange@ur.rochester.edu>
2016-11-03 10:18:54 -0400

    convertToSmallImageSize takes file names in and does the saving

--- convertToSmallImageSize.m
+++ convertToSmallImageSize.m
@@ -1,27 +1,31 @@
-function [Data, small_image_collection] = convertToSmallImageSize(Data, image_collection)
+function small_contents = convertToSmallImageSize(old_file, new_file, data_field, image_field)
 
-[trials, frames, ~, ~] = size(image_collection);
+contents = load(old_file);
+small_contents = contents;
 
-small_height = Data.image_length_y;
-small_width  = Data.image_length_x;
+[trials, frames, ~, ~] = size(contents.(image_field));
 
-%% Resize images in image_collection
+small_height = contents.(data_field).image_length_y;
+small_width  = contents.(data_field).image_length_x;
 
-small_image_collection = zeros(trials, frames, small_height, small_width);
+%% Resize images in contents.(image_field)
+small_contents.(image_field) = zeros(trials, frames, small_height, small_width);
 
 for t=1:trials
     for f=1:frames
-        small_image_collection(t, f, :, :) = ...
-            imresize(squeeze(image_collection(t, f, :, :)), ...
+        small_contents.(image_field)(t, f, :, :) = ...
+            imresize(squeeze(contents.(image_field)(t, f, :, :)), ...
             [small_height, small_width], 'nearest');
     end
 end
 
 %% Resize templates
-
-if isfield(Data, 'left_template')
-    Data.left_template  = imresize(Data.left_template,  [small_height, small_width], 'nearest');
-    Data.right_template = imresize(Data.right_template, [small_height, small_width], 'nearest');
+if isfield(contents.(data_field), 'left_template')
+    small_contents.(data_field).left_template  = imresize(contents.(data_field).left_template,  [small_height, small_width], 'nearest');
+    small_contents.(data_field).right_template = imresize(contents.(data_field).right_template, [small_height, small_width], 'nearest');
 end
+
+%% Save converted data to the new file
+save(new_file, '-struct', 'small_contents');
 
 end
