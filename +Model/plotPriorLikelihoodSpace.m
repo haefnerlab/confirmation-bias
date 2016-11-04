4e8b61f6abd22994638987b368eab27191acbaaa
wrongu <rlange@ur.rochester.edu>
2016-11-03 22:10:21 -0400

    plotPLSpace using 1/variance measure of information and takes var_x into account

--- +Model/plotPriorLikelihoodSpace.m
+++ +Model/plotPriorLikelihoodSpace.m
@@ -1,49 +1,69 @@
-function [correct] = plotPriorLikelihoodSpace(trials, frames, priors, likelihoods, sampling_params, ideal_observer)
+function [correct] = plotPriorLikelihoodSpace(trials, frames, I_prior, I_likelihood, params, ideal_observer)
+%PLOTPRIORLIKELIHOODSPACE make a info_prior vs info_likelihood plot for the
+%given params.
+%
+%params.p_match and params.var_e will be set based on alpha and beta.
 
 savedir = fullfile('+Model', 'figures');
 if ~exist(savedir, 'dir'), mkdir(savedir); end
 
-if nargin < 6, ideal_observer = false; end
+if nargin < 7, ideal_observer = false; end
 
-% Get cartesian product of priors x likelihoods
-[ll, pp] = meshgrid(likelihoods, priors);
-correct = zeros(size(pp));
-pp = pp(:);
-ll = ll(:);
+% Check for bounds on alpha and beta imposed by var_x
+[alpha_min, beta_max] = getInformationBounds(params.var_x);
 
-parfor i=1:numel(pp)
-    prior = pp(i);
-    likelihood = ll(i);
-    [data, var_e, prefix] = Model.genDataWithPriorLikelihood(trials, frames, prior, likelihood);
-    sampling_params_copy = sampling_params;
-    sampling_params_copy.var_e = var_e;
-    sampling_params_copy.p_match = prior;
+% Get meshgrid of alpha:beta for both the prior (y) and likelihood (x)
+% axes.
+[I_L, I_P] = meshgrid(I_likelihood, I_prior);
+
+correct = nan(size(I_P));
+
+parfor i=1:numel(I_P)
+    if I_P(i) > beta_max || I_P(i) < alpha_min, continue; end
+    params_copy = params;
+    % Set variances for given 'information' contents.
+    params_copy.var_e = 1 / I_L(i);
+    params_copy.p_match = solveForPriorParams(I_P(i), params.var_x);
+    
+    % Generate data and run the model.
+    [data, prefix] = Model.genDataWithParams(trials, frames, params_copy);
     if ~ideal_observer
-        results = Model.loadOrRunSamplingModel(data, prefix, sampling_params_copy);
+        results = Model.loadOrRunSamplingModel(data, prefix, params_copy);
     else
-        results = Model.runIdealObserver(data, sampling_params_copy);
+        results = Model.runIdealObserver(data, params_copy);
     end
     correct(i) = sum(results.choices == +1) / trials;
 end
 
-sub_prior = 1:5:length(priors);
-sub_like = 1:5:length(likelihoods);
-
 imagesc(correct, [0.5 1.0]); axis image; colorbar;
-set(gca, 'XTick', sub_like);
-set(gca, 'YTick', sub_prior);
-set(gca, 'XTickLabel', arrayfun(@num2str, likelihoods(sub_like), 'UniformOutput', false));
-set(gca, 'YTickLabel', arrayfun(@num2str, priors(sub_prior), 'UniformOutput', false));
+prior_tick_indices = round(linspace(1, length(I_prior), min(length(I_prior), 5)));
+like_tick_indices = round(linspace(1, length(I_likelihood), min(length(I_likelihood), 5)));
+set(gca, 'YTick', prior_tick_indices);
+set(gca, 'XTick', like_tick_indices);
+set(gca, 'YTickLabel', arrayfun(@num2str, I_prior(prior_tick_indices), 'UniformOutput', false));
+set(gca, 'XTickLabel', arrayfun(@num2str, I_likelihood(like_tick_indices), 'UniformOutput', false));
 set(gca, 'YDir', 'Normal');
-xlabel('P_{likelihood}');
-ylabel('P_{prior}');
-title('Percent Correct in prior-likelihood space');
+xlabel('Info_{likelihood}');
+ylabel('Info_{prior}');
+title('PL-Space');
 if ~ideal_observer
     figname = sprintf('PLSpace_%dx%d_vx%.2f_pD%.2f_gam%.2f_S%d.fig', ...
-        trials, frames, sampling_params.var_x, sampling_params.prior_D, ...
-        sampling_params.gamma, sampling_params.samples);
+        trials, frames, params.var_x, params.prior_D, ...
+        params.gamma, params.samples);
 else
     figname = sprintf('PLSpace_%dx%d_ideal.fig', trials, frames);
 end
 saveas(gcf, fullfile(savedir, figname));
+end
+
+function [alpha_min, beta_max] = getInformationBounds(var_x)
+% There is a limit to the variance that can be contributed by p_match at a
+% given value of var_x. This function returns the low and high bounds on
+% information.
+alpha_min = 1 / (var_x + 0.5); % maximal variance of 2*0.25 when p is 0.5.
+beta_max = 1 / var_x; % minimal variance of var_x when p is 1.
+end
+
+function p_match = solveForPriorParams(info_prior, var_x)
+p_match = (1 + sqrt(1 + 2*(var_x + 1/info_prior))) / 2;
 end
