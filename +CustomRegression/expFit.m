49999f0eec3037fe708486dedcb1c42455706ea9
wrongu <rlange@ur.rochester.edu>
2016-11-09 11:33:02 -0500

    expFit uses gradient

--- +CustomRegression/expFit.m
+++ +CustomRegression/expFit.m
@@ -1,14 +1,21 @@
 function expfit = expFit(weights, errors)
 if nargin < 2, errors = ones(size(weights)); end
 
-xs = (1:length(weights))';
-
-expfn = @(x, p) p(1) + p(2) * exp(-x / p(3));
-
-errfn = @(p) sum((weights - expfn(xs, p)).^2 ./ errors);
-
 p0 = [mean(weights) 0 10];
 
-expfit = fmincon(errfn, p0, [], [], [], [], [0 -inf 0], [inf inf inf]);
+expfit = fmincon(@(p) errfn(p, weights, errors), p0, [], [], [], [], [0 -inf 0], [inf inf inf]);
 
+end
+
+function [err, grad] = errfn(p, w, e)
+xs = (1:length(w))';
+expvals = p(1) + p(2) * exp(-xs / p(3));
+err = sum((w - expvals).^2 ./ e);
+
+if nargout >= 2
+    gradA = -2 * sum((w - expvals) ./ e);
+    gradB = 2 * sum((w - expvals) .* exp(-xs / p(3)) ./ e);
+    gradTau = 2 * sum((w - expvals) ./ e .* (p(2) * exp(-xs / p(3)) / p(3)^2));
+    grad = [gradA gradB gradTau];
+end
 end
