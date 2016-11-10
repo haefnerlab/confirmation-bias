aca62e7f7f962ccf24b70da55ce6ce5db493c098
wrongu <rlange@ur.rochester.edu>
2016-11-10 07:56:41 -0500

    noise term in showExampleImages

--- showExampleImages.m
+++ showExampleImages.m
@@ -1,9 +1,10 @@
-function showExampleImages(n_images, contrast, p_match)
+function showExampleImages(n_images, contrast, noise, p_match)
 Data.image_length_y = 5;
 Data.image_length_x = 5;
 Data.number_of_images = n_images;
 Data.left_template = eye(5);
 Data.right_template = rot90(Data.left_template);
+Data.pixel_noise = noise;
 
 order = sign(p_match - rand(1:n_images));
 
@@ -11,7 +12,7 @@ images = makeImages(Data, order, contrast) / 256;
 
 for i=1:n_images
     subplot(1, n_images, i);
-    imagesc(squeeze(images(i, :, :))); colormap gray; axis image;
+    imagesc(squeeze(images(i, :, :)), [0 1]); colormap gray; axis image;
     xticks([]); yticks([]);
 end
 end
