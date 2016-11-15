3f40b0d4f774300883ee8c0b1906973a0ba2c6ba
wrongu <rlange@ur.rochester.edu>
2016-11-15 13:24:50 -0500

    RT and timeout in ptbWaitKey

--- ptbWaitKey.m
+++ ptbWaitKey.m
@@ -1,6 +1,15 @@
-function key = ptbWaitKey(allowed_keys)
+function [key, reaction_time, timeout] = ptbWaitKey(allowed_keys, timelimit)
+if nargin < 2, timelimit = 0; end
+timeout = false;
+tstart = tic;
 [~, ~, key] = KbCheck(-1);
 while ~any(key == allowed_keys)
     [~, ~, key] = KbCheck(-1);
+    if timelimit > 0 && toc(tstart) > timelimit
+        timeout = true;
+        break;
+    end
 end
+reaction_time = toc(tstart);
+key = intersect(find(key), allowed_keys);
 end
