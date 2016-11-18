cde5a33845efb1b9b385b7ca248547b8b0c924f7
wrongu <rlange@ur.rochester.edu>
2016-11-17 21:14:53 -0500

    fixed bug in pixel noise staircase

--- +GaborStaircase/pixel_noise.m
+++ +GaborStaircase/pixel_noise.m
@@ -38,11 +38,11 @@ end
 if GaborData.streak(trial) == 0
     % Got it wrong - make things easier
     GaborData.pixel_noise(trial) = ...
-        GaborData.step_size(trial) + GaborData.pixel_noise(trial-1);
+        GaborData.pixel_noise(trial-1) + GaborData.step_size(trial);
 elseif mod(GaborData.streak(trial), 2) == 0
     % Got 2 right in a row - make things harder
     GaborData.pixel_noise(trial) = ...
-        GaborData.step_size(trial) - GaborData.pixel_noise(trial-1);
+        GaborData.pixel_noise(trial-1) - GaborData.step_size(trial);
 end
 
 % Apply bounds
