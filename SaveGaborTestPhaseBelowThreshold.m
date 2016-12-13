43bc998592c5515d76e1e92aca95d12968bf27a0
wrongu <rlange@ur.rochester.edu>
2016-12-13 14:24:55 -0500

    updated Save/Concat/etc to use new GaborData fields

--- SaveGaborTestPhaseBelowThreshold.m
+++ SaveGaborTestPhaseBelowThreshold.m
@@ -23,7 +23,7 @@ else
     contents = load(filename); % Load Preliminary_Data and image_collection
 end
 
-Data = contents.Preliminary_Data;
+Data = contents.GaborData;
 image_collection = contents.image_collection;
 
 test_trials = Data.(stair_param) < threshold;
@@ -35,33 +35,28 @@ image_collection_test = image_collection(test_trials, :, :, :);
 
 %% Copy from Data to Test_Data
 
-Test_Data.move_on = Data.move_on(test_trials);
-Test_Data.step_size = Data.step_size(test_trials);
-Test_Data.reversal_counter = Data.reversal_counter(test_trials);
+Test_Data = Data;
+
+Test_Data.current_trial = elements;
+
 Test_Data.contrast = Data.contrast(test_trials);
 Test_Data.ratio = Data.ratio(test_trials);
+Test_Data.pixel_noise = Data.pixel_noise(test_trials);
+Test_Data.step_size = Data.step_size(test_trials);
+
+Test_Data.streak = Data.streak(test_trials);
+Test_Data.reversal_counter = Data.reversal_counter(test_trials);
 Test_Data.correct_answer = Data.correct_answer(test_trials);
-Test_Data.staircase_answer = Data.staircase_answer(test_trials);
+Test_Data.ideal_answer = Data.ideal_answer(test_trials);
 Test_Data.reaction_time = Data.reaction_time(test_trials);
 Test_Data.choice = Data.choice(test_trials);
 Test_Data.accuracy = Data.accuracy(test_trials);
 Test_Data.order_of_orientations = Data.order_of_orientations(test_trials, :);
-Test_Data.log_odds = Data.log_odds(test_trials);
+Test_Data.log_frame_odds = Data.log_frame_odds(test_trials, :);
+Test_Data.log_decision_odds = Data.log_decision_odds(test_trials, :);
 Test_Data.average_orientations = Data.average_orientations(:, test_trials);
-Test_Data.image_template1 = Data.image_template1(test_trials, :);
-Test_Data.image_template2 = Data.image_template2(test_trials, :);
-Test_Data.image_template_difference = Data.image_template_difference(test_trials, :);
-%Test_Data.eye_tracker_points = Data.eye_tracker_points(test_trials);
 
-Test_Data.current_trial = elements;
-Test_Data.screen_frame = Data.screen_frame;
-Test_Data.screen_resolution = Data.screen_resolution;
-Test_Data.image_length_x = Data.image_length_x;
-Test_Data.image_length_y = Data.image_length_y;
-Test_Data.left_template = Data.left_template;
-Test_Data.right_template = Data.right_template;
-Test_Data.number_of_images = Data.number_of_images;
-Test_Data.pixel_noise = Data.pixel_noise;
+Test_Data.eye_tracker_points = Data.eye_tracker_points(test_trials);
 
 %% Save Test_Data and image_collection_test
 
--- SaveQuitDataGabor.m
+++ SaveQuitDataGabor.m
@@ -1,76 +1,48 @@
-%function [] = SaveQuitDataGabor(subjectID, phase, loops_consider, preliminary_trials,extra, directory)
-function [] = SaveQuitDataGabor(subjectID, phase, directory)
+function SaveQuitDataGabor(subjectID, phase, directory)
 %% Very important to remember that you should save till BEFORE current trial and not upto current trial!!That is consider_trials<current_trial
 
-if phase == 0       % load the volume data
-    filename = fullfile(directory, 'RawData', [subjectID '-GaborDataContrast.mat']);
-    if ~exist(filename, 'file')
-        disp(strcat('ERROR! Missing File: ', filename));  % Return an error message for missing file
-        disp(strcat('Maybe the Preliminary phase is saved under a different name?'));
-        return;
-    else
-        load(filename); % Load Preliminary_Data
-    end
-elseif phase == 1       % load the ratio data
-    filename = fullfile(directory, 'RawData', [subjectID '-GaborDataRatio.mat']);
-    if ~exist(filename, 'file')
-        disp(strcat('ERROR! Missing File: ', filename));  % Return an error message for missing file
-        disp(strcat('Maybe the Preliminary phase is saved under a different name?'));
-        return;
-    else
-        load(filename); % Load Preliminary_Data
-    end
+if phase == 0
+    expt_type = 'Contrast';
+elseif phase == 1
+    expt_type = 'Ratio';
+else
+    error('Unknown phase: %d', phase);
 end
 
-%point = loops_consider*preliminary_trials+extra;
-point=Preliminary_Data.current_trial-5;
-Preliminary_Data.move_on (point+1:end) = [];          % Is the subject ready to move on or not? Always 0 or 1 for how many trials they got right so far
-Preliminary_Data.step_size (point+1:end) = [];        % By how much to adjust the contrast [1.5, 1.2, or 1.1]
-Preliminary_Data.reversal_counter (point+1:end) = [];   % How many trials has the subject got wrong? When to change the step size?
-Preliminary_Data.contrast(point+1:end) = [];         % How loud the sound is, or the signal level
-Preliminary_Data.number_of_images = 10;
-Preliminary_Data.correct_answer(point+1:end) = [];         % What was the right ear/answer?
-Preliminary_Data.staircase_answer(point+1:end) = [];         % What was the right ear/answer?
-Preliminary_Data.reaction_time(point+1:end) = [];          % How quick is the subject to input their answer for the orientation choice? Recorded in ms
-Preliminary_Data.choice(point+1:end) = [];                 % Which ear did the subject choose? 1 = left, 0 = right
-Preliminary_Data.accuracy(point+1:end) = [];               % Did the subject make the right selection? 1 = yes, 0 = no
-Preliminary_Data.order_of_orientations((point+1:end),:) = [];        % Record the randomized order of image orientations throughout the trial
-Preliminary_Data.log_odds(point+1:end) = [];
-Preliminary_Data.ratio(point+1:end) = [];
-Preliminary_Data.average_orientations(:,point+1) = [];
+filename = fullfile(directory, 'RawData', sprintf('%s-GaborData%s.mat', subjectID, expt_type));
+filenameQuit = fullfile(directory, 'RawData', sprintf('%s-GaborData%sQuit.mat', subjectID, expt_type));
+if ~exist(filenameQuit, 'file')
+    error('ERROR! Missing File: %s\nMaybe the Preliminary phase is saved under a different name?', filenameQuit);
+else
+    contents = load(filenameQuit);
+    GaborData = contents.GaborData;
+    image_collection = contents.image_collection;
+end
 
-Preliminary_Data.current_trial = 0;
+% Truncate per-trial fields
+point = GaborData.current_trial-2;
+GaborData.current_trial = point;
 
-%Preliminary_Data.eye_tracker_info=
-Preliminary_Data.screen_frame = 12;	% how long each image will show on screen in frame rates
-Preliminary_Data.screen_resolution = 25;          % how many pixels correspond to a single datapoint of a gabor
-Preliminary_Data.image_length_x = 5;  % Size of the image along x-axis
-Preliminary_Data.image_length_y = 5;
+GaborData.contrast(point+1:end) = [];
+GaborData.ratio(point+1:end) = [];
+GaborData.pixel_noise(point+1:end) = [];
+GaborData.step_size (point+1:end) = [];
 
-Preliminary_Data.image_template1(point+1:end,:)=[];
-Preliminary_Data.image_template2(point+1:end,:)=[];
-Preliminary_Data.image_template_difference(point+1:end,:)=[];
+GaborData.streak (point+1:end) = [];
+GaborData.reversal_counter (point+1:end) = [];
+GaborData.correct_answer(point+1:end) = [];
+GaborData.ideal_answer(point+1:end) = [];
+GaborData.reaction_time(point+1:end) = [];
+GaborData.choice(point+1:end) = [];
+GaborData.accuracy(point+1:end) = [];
+GaborData.order_of_orientations(point+1:end,:) = [];
+GaborData.log_frame_odds(point+1:end,:) = [];
+GaborData.log_decision_odds(point+1:end,:) = [];
+GaborData.average_orientations(:,point+1) = [];
 
+GaborData.eye_tracker_points(point+1:end) = [];
 
 image_collection(point+1:end,:,:,:)=[];
 
-if phase==0
-    if ~exist(directory, 'dir') % Check the directory actually exists
-        mkdir(directory);
-        fileName = fullfile(directory, 'RawData', [subjectID '-GaborDataContrast.mat']); % create a name for the data you want to save
-        save(fileName, 'Preliminary_Data','image_collection'); % save the data
-    else
-        fileName = fullfile(directory, 'RawData', [subjectID '-GaborDataContrast.mat']); % create a name for the data you want to save
-        save(fileName, 'Preliminary_Data','image_collection'); % save the data
-    end
-elseif phase==1
-    if ~exist(directory, 'dir') % Check the directory actually exists
-        mkdir(directory);
-        fileName = fullfile(directory, 'RawData', [subjectID '-GaborDataRatio.mat']); % create a name for the data you want to save
-        save(fileName, 'Preliminary_Data','image_collection'); % save the data
-    else
-        fileName = fullfile(directory, 'RawData', [subjectID '-GaborDataRatio.mat']); % create a name for the data you want to save
-        save(fileName, 'Preliminary_Data','image_collection'); % save the data
-    end
-end
+save(filename, 'GaborData','image_collection');
 end
