6dd7c010f6db031d517aa922f92f653c02093908
wrongu <rlange@ur.rochester.edu>
2017-01-03 16:07:32 -0500

    'optimize' option in Model PL-space functions

--- +Model/plotPLPK.m
+++ +Model/plotPLPK.m
@@ -1,19 +1,31 @@
-function plotPLPK(trials, frames, prior, likelihood, params, ideal_observer, pk_hprs, pk_colormap)
+function plotPLPK(trials, frames, prior, likelihood, params, ideal_observer, pk_hprs, optimize, pk_colormap)
 
 savedir = fullfile('+Model', 'figures');
 if ~exist(savedir, 'dir'), mkdir(savedir); end
+if nargin < 7, optimize = {}; end
 
 % Keep in sync with plotPLSpace
-if ~ideal_observer
-    figname = sprintf('PLSpace_%dx%d_vx%.2f_pD%.2f_gam%.2f_ns%d_b%d.fig', ...
-        trials, frames, params.var_x, params.prior_D, ...
-        params.gamma, params.samples, params.batch);
+if isempty(optimize)
+    if ~ideal_observer
+        figname = sprintf('PLSpace_%dx%d_vx%.2f_pD%.2f_gam%.2f_ns%d_b%d.fig', ...
+            trials, frames, params.var_x, params.prior_D, ...
+            params.gamma, params.samples, params.batch);
+    else
+        figname = sprintf('PLSpace_%dx%d_vx%.2f_ideal.fig', trials, frames, params.var_x);
+    end
+    if ~exist(fullfile(savedir, figname), 'file')
+        Model.plotPriorLikelihoodSpace(trials, frames, prior, likelihood, params, ideal_observer);
+        close all;
+    end
 else
-    figname = sprintf('PLSpace_%dx%d_vx%.2f_ideal.fig', trials, frames, params.var_x);
-end
-if ~exist(fullfile(savedir, figname), 'file')
-    Model.plotPriorLikelihoodSpace(trials, frames, prior, likelihood, params, ideal_observer);
-    close all;
+    figname = sprintf('PLSpace_%dx%d_optimized_vx%.2f_pD%.2f_gam%.2f_ns%d_b%d.fig', ...
+            trials, frames, params.var_x, params.prior_D, ...
+            params.gamma, params.samples, params.batch);
+    if ~exist(fullfile(savedir, figname), 'file')
+        % Get PL-space data for optimized params
+        Model.plotPriorLikelihoodSpace(trials, frames, prior, likelihood, params, false, false, optimize);
+        close all;
+    end
 end
 
 pl_fig = openfig(fullfile(savedir, figname));
@@ -29,7 +41,7 @@ pri_pts = round(100*interp1(1:length(prior), prior, y))/100;
 pk_fig = figure;
 pk_ax = axes(pk_fig);
 
-if nargin < 7
+if nargin < 9
     % create pk_colormap that is dark blue -> dark red
     fade = linspace(0, 1, npts)';
     colors = [fade*170/255, zeros(size(fade)), (1-fade)*170/255];
