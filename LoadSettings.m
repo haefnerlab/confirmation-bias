2764bb2843a93d77ef77ff6daab6dbb448e50150
wrongu <rlange@ur.rochester.edu>
2017-02-15 19:08:36 -0500

    fixed monitorFPS sometimes 0

--- LoadSettings.m
+++ LoadSettings.m
@@ -22,6 +22,10 @@ if ~isfield(settings, 'ptbSkipSyncTests'), settings.ptbSkipSyncTests = 0; end
 trueSize = Screen('Resolution', settings.whichScreen);
 if ~isfield(settings, 'screenSize'), settings.screenSize = [0 0 trueSize.width trueSize.height]; end
 settings.monitorFPS = Screen('NominalFrameRate', settings.whichScreen, 1);
+if settings.monitorFPS == 0
+    warning('Getting frame rate failed. Assuming exactly 60.0 FPS');
+    settings.monitorFPS = 60.0;
+end
 % Interface settings - which keys do what
 if ~isfield(settings, 'keyGo'), settings.keyGo = 'space'; end
 if ~isfield(settings, 'keyGoName'), settings.keyGoName = 'the space bar'; end
