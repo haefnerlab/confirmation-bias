1c06e6e0b3d2a966a88bb526ed95c280d7325531
wrongu <rlange@ur.rochester.edu>
2017-02-19 19:49:05 -0500

    updated plotBiasVariance.m (but results still don't make sense)

--- +Model/plotBiasVariance.m
+++ +Model/plotBiasVariance.m
@@ -1,4 +1,4 @@
-function plotBiasVariance(trials, frames, prior, likelihood, sampling_params, gammas)
+function plotBiasVariance(trials, frames, sampling_params, gammas)
 %PLOTBIASVARIANCE Run sampling model with different gammas to show bias
 %variance tradeoff.
 
@@ -9,12 +9,19 @@ for i=1:length(gammas)
     gamma = gammas(i);
     params_copy = sampling_params;
     params_copy.gamma = gamma;
-    [data, params_copy.var_e, prefix] = Model.genDataWithPriorLikelihood(trials, frames, prior, likelihood);
+    [data, prefix] = Model.genDataWithParams(trials, frames, sampling_params);
     [results, data] = Model.loadOrRunSamplingModel(data, prefix, params_copy);
     [ideal_results] = Model.runIdealObserver(data, params_copy);
+
+    % Collapse together multiple-samples of model.
+    model_walk = zeros(size(ideal_results.walk));
+    for t=1:trials
+        batched_samples_walk = reshape(results.walk(t,2:end), [sampling_params.samples, frames]);
+        model_walk(t,:) = [results.walk(1) mean(batched_samples_walk, 1)];
+    end
     
     % Get difference between sampling log posterior and ideal log posterior
-    log_posterior_difference = results.walk - ideal_results.walk;
+    log_posterior_difference = model_walk - ideal_results.walk;
     [M, L, U] = meanci(log_posterior_difference);
     
     stringID = Model.getModelStringID(prefix, params_copy);
