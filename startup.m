609978b00f1887f4f50862c99a5614b75372a278
wrongu <rlange@ur.rochester.edu>
2017-04-16 10:15:16 -0400

    updated tools/ submodule

--- startup.m
+++ startup.m
@@ -1,4 +1,4 @@
 if ~exist('psignifit', 'file'), addpath('psignifit-master'); end
 if ~exist('smoothn', 'file'), addpath('smoothn'); end
 if ~exist('fadecolors', 'file'), addpath('tools'); end
-
+if ~exist('boundedline', 'file'), addpath(fullfile('tools', 'boundedline-pkg', 'boundedline')); end
