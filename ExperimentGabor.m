ff5740c702adf8b60527e5728c2374b2b542a7be
wrongu <rlange@ur.rochester.edu>
2017-04-16 10:20:33 -0400

    fraction_blank --> blank_frames

--- ExperimentGabor.m
+++ ExperimentGabor.m
@@ -70,15 +70,7 @@ if exist(fileName, 'file')
 end
 
 %% Timing calculations and checks
-
-monitor_frames_per_stimulus = settings.monitorFPS / GaborData.stimulus_fps;
-number_blank = monitor_frames_per_stimulus * GaborData.fraction_blank;
-rounded_number_blank = ceil(number_blank);
-if number_blank ~= rounded_number_blank
-    warning('At %d stimulus fps on a %.2fHz monitor, %.2f fraction_blank requires rounding to %d blank frames per stimulus.', ...
-        GaborData.stimulus_fps, settings.monitorFPS, GaborData.fraction_blank, rounded_number_blank);
-end
-GaborData.blank_duration = rounded_number_blank / settings.monitorFPS;
+GaborData.blank_duration = settings.monitorFPS * GaborData.blank_frames;
 
 %% Begin experiment
 EyeTracker.AutoCalibrate(tracker_info);
