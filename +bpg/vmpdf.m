f9a4ff22ca93f177f87a25c468086bb097445621
wrongu <rlange@ur.rochester.edu>
2017-04-17 18:50:36 -0400

    fixed bpg.vmpdf at kappa 0

--- +bpg/vmpdf.m
+++ +bpg/vmpdf.m
@@ -1,5 +1,9 @@
 function p = vmpdf(x, theta, kappa)
 %VMPDF Von-Mises circular pdf. Theta is mean, kappa controls width. Inputs
 %are in radians.
-p = exp(kappa * cos(x - theta)) / besseli(0, kappa);
+if kappa == 0
+    p = 1 / (2 * pi);
+else
+    p = exp(kappa * cos(x - theta)) / besseli(0, kappa);
+end
 end
