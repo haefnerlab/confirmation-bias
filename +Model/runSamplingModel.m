a6e19bc2c0451321cd88a5d60bd1bf0dcc97154c
wrongu <rlange@ur.rochester.edu>
2017-11-29 11:03:16 -0500

    fixed importance_norm order of operations

--- +Model/runSamplingModel.m
+++ +Model/runSamplingModel.m
@@ -76,7 +76,10 @@ for i=1:trials
             xs = mog.sample(Q, batch);
             results.x(i, s_idx:s_idx+batch-1) = xs;
             % Update accumulated evidence using importance-sampling weights
-            ws = 1 ./ mog.pdf(xs, prior, params.importance_norm);
+            ws = 1 ./ mog.pdf(xs, prior);
+            if params.importance_norm
+                ws = ws / sum(ws);
+            end
             results.w(i, s_idx:s_idx+batch-1) = ws;
             update = log(dot(mog.pdf(xs, p_x_Cp), ws)) - ...
                 log(dot(mog.pdf(xs, p_x_Cm), ws));
