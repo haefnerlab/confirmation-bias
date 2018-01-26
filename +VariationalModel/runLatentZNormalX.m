9bf6a125abdc20d09a06d9068e9b08c19e96cdff
wrongu <rlange@ur.rochester.edu>
2018-01-26 11:30:59 -0500

    1st commit of +VariationalModel with runLatentZNormalX and C/S Space plots

---
+++ +VariationalModel/runLatentZNormalX.m
@@ -0,0 +1,58 @@
+function [pC, data, choice] = runLatentZNormalX(params)
+% Variational Bayes inference in a model where C=+/-1, p(x|C) is a gaussian centered on +/-C with
+% some probability, and p(s|x) is also gaussian. This version assumes a posterior that factorizes
+% x,z and c, i.e.
+%    q(x,z,c)=q(x)*q(z)*q(c)
+
+trials = params.trials;
+frames = params.frames;
+var_s = params.var_s;
+var_x = params.var_x;
+noise = params.noise;
+gamma = params.gamma;
+updates = params.updates;
+
+pC = zeros(trials, frames + 1);
+pC(1:trials, 1) = params.prior_C;
+
+% Generate the stimulus, all with C=+1 'correct'
+data = SamplingModel.genDataWithParams(params);
+
+pz0 = params.p_match;
+
+for t=1:frames
+    % implement VB to get t+1 probabilities
+    prior_C = pC(:, t); % Use last posterior as new prior
+    log_prior_odds_C = log(prior_C) - log(1 - prior_C);
+    pz = repmat(pz0, trials, 1);
+    
+    for i=1:updates
+        % Infer p(x|z,c,s)
+        expected_cz = (2 * prior_C - 1) .* (2 * pz - 1);
+        mu_x = (expected_cz * var_s + data(:, t) * var_x) / (var_s + var_x);
+        % Note: updated var_x is constant: (1/var_x + 1/var_s)^-1
+
+        % Infer p(z|x,c)
+        % JEFF
+        % log_odds_z = (2 * priorC - 1) .* mu_x / var_x + log(pz0) - log(1-pz0);
+        % RICHARD
+        log_odds_z = 2 * (2 * prior_C - 1) .* mu_x / var_x + log(pz0) - log(1-pz0);
+        pz = 1 ./ (1 + exp(-log_odds_z));
+        
+        % Infer updated p(c|s)
+        % JEFF
+        % log_odds_C = (2 * pz - 1) .* mu_x / var_x + log_prior_odds_C;
+        % RICHARD
+        log_odds_C = 2 * (2 * pz - 1) .* mu_x / var_x + log_prior_odds_C * (1 - gamma);
+        
+        % Add multiplicative noise to accumulated log probability
+        if noise > 0
+            log_odds_C = log_odds_C .* exp(noise * randn(trials,1));
+        end
+        pC(:, t+1) = 1 ./ (1 + exp(-log_odds_C)); 
+    end
+end
+
+% compute decision variable
+choice = pC(:, frames+1) > .5;
+end
