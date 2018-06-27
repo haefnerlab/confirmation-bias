34ae2140a9ff8a4e67a987d10d72ba3d37986c24
wrongu <rlange@ur.rochester.edu>
2018-06-27 16:17:47 -0400

    GaborAnalysis/DeltaSlopeStatistics.m takes 'linear' or 'exponential' as an input

--- +GaborAnalysis/DeltaSlopeStatistics.m
+++ +GaborAnalysis/DeltaSlopeStatistics.m
@@ -1,4 +1,4 @@
-function [slopes, pvalues] = DeltaSlopeStatistics(subjectIds, phases, nboot, datadir)
+function [slopes, pvalues] = DeltaSlopeStatistics(subjectIds, phases, type, nboot, datadir)
 %DELTASLOPESTATISTICS use linear PK fit to each of the two phases, bootstrapped, and return (1) the
 %[num subjects x 2 x bootstraps] array of PK slopes and (2) the [num subjects] array of p-values for
 %there being a significant difference between the two phases.
@@ -19,62 +19,84 @@ window_low = 0.5;
 window_high = 0.75;
 
     function [floor, thresh] = getThresholdWrapper(subjectId, phase)
-    if phase == 1
-        floor = .4;
-        thresh = .6;
-    else
-        [floor, thresh] = GaborAnalysis.getThresholdWindow(subjectId, phase, window_low, window_high, datadir);
-    end
+        if phase == 1
+            floor = .4;
+            thresh = .6;
+        else
+            [floor, thresh] = GaborAnalysis.getThresholdWindow(subjectId, phase, window_low, window_high, datadir);
+        end
     end
 
     function [stair_var, stair_var_name] = getStairVar(phase)
-    if phase == 0
-        stair_var = 'contrast';
-        stair_var_name = 'contrast';
-    elseif phase == 1
-        stair_var = 'true_ratio';
-        stair_var_name = 'ratio';
-    elseif phase == 2
-        stair_var = 'noise';
-        stair_var_name = 'noise';
-    else
-        error('Expected phase 0 for Contrast or 1 for Ratio or 2 for Noise');
-    end
+        if phase == 0
+            stair_var = 'contrast';
+            stair_var_name = 'contrast';
+        elseif phase == 1
+            stair_var = 'true_ratio';
+            stair_var_name = 'ratio';
+        elseif phase == 2
+            stair_var = 'noise';
+            stair_var_name = 'noise';
+        else
+            error('Expected phase 0 for Contrast or 1 for Ratio or 2 for Noise');
+        end
     end
 
-    function fits = doFitsForPhase(subjectId, phase)
-    stair_var = getStairVar(phase);
-    
-    SubjectData = LoadOrRun(@LoadAllSubjectData, ...
-        {subjectId, phase, datadir}, fullfile(catdir, [subjectId '-' stair_var '.mat']));
-    [floor, thresh] = getThresholdWrapper(subjectId, phase);
-    SubjectDataThresh = GaborThresholdTrials(SubjectData, phase, thresh, floor);
-    
-    memo_name = ['Boot-LinPK-ideal-' stair_var '-' subjectId '-' num2str(thresh) '-' num2str(floor) '.mat'];
-    [~, ~, ~, ~, ~, fits] = LoadOrRun(@BootstrapLinearPKFit, {SubjectDataThresh, nboot}, ...
-        fullfile(memodir, memo_name));
+    function slopes = getSlopesForPhase(subjectId, phase)
+        stair_var = getStairVar(phase);
+        
+        SubjectData = LoadOrRun(@LoadAllSubjectData, ...
+            {subjectId, phase, datadir}, fullfile(catdir, [subjectId '-' stair_var '.mat']));
+        [floor, thresh] = getThresholdWrapper(subjectId, phase);
+        SubjectDataThresh = GaborThresholdTrials(SubjectData, phase, thresh, floor);
+        
+        switch type
+            case 'linear'
+                memo_name = ['Boot-LinPK-ideal-' stair_var '-' subjectId '-' num2str(thresh) '-' num2str(floor) '.mat'];
+                [~, ~, ~, ~, ~, fits] = LoadOrRun(@BootstrapLinearPKFit, {SubjectDataThresh, nboot}, ...
+                    fullfile(memodir, memo_name));
+                slopes = fits(:, 1);
+            case 'exponential'
+                memo_name = ['Boot-ExpPK-ideal-' stair_var '-' subjectId '-' num2str(thresh) '-' num2str(floor) '.mat'];
+                [~, ~, ~, ~, fits] = LoadOrRun(@BootstrapExponentialWeightsGabor, {SubjectDataThresh, nboot}, ...
+                    fullfile(memodir, memo_name));
+                slopes = fits(:, 2);
+        end
     end
 
     function name = cleanName(subjectId)
-    st = regexpi(subjectId, 'subject\d+');
-    name = subjectId(st:end);
+        st = regexpi(subjectId, 'subject\d+');
+        name = subjectId(st:end);
     end
 
-edges = -.2:.005:.2;
+switch type
+    case 'linear'
+        xlab = 'linear slope';
+        range = [-.2 .2];
+    case 'exponential'
+        xlab = '\beta';
+        range = [-1 1];
+end
+
+figure;
+edges = range(1):.005:range(2);
 edgediff = edges(2) - edges(1);
+all_slopes_diff = zeros(length(subjectIds), nboot);
+all_slopes_diff_medians = zeros(length(subjectIds), 1);
 for s_idx=1:length(subjectIds)
     %% Compute fits for subject
-    fits1 = doFitsForPhase(subjectIds{s_idx}, phases(1));
-    slopes1 = fits1(:, 1);
+    slopes1 = getSlopesForPhase(subjectIds{s_idx}, phases(1));
     slopes(s_idx, 1, :) = slopes1;
     
-    fits2 = doFitsForPhase(subjectIds{s_idx}, phases(2));
-    slopes2 = fits2(:, 1);
+    slopes2 = getSlopesForPhase(subjectIds{s_idx}, phases(2));
     slopes(s_idx, 2, :) = slopes2;
     
     %% Take difference of slopes and compute pvalue
     slopes_diff = slopes1 - slopes2;
     pvalues(s_idx) = (1 + sum(slopes_diff < 0)) / (1 + nboot);
+    
+    all_slopes_diff(s_idx, :) = slopes_diff;
+    all_slopes_diff_medians(s_idx) = median(slopes_diff);
     
     %% Plot histograms
     subplotsquare(length(subjectIds), s_idx);
@@ -90,12 +112,12 @@ for s_idx=1:length(subjectIds)
     
     hist1 = histcounts(slopes1, edges);
     hist2 = histcounts(slopes2, edges);
-    bar(edges(1:end-1), hist1, .5, 'r', 'EdgeColor', 'none');
-    bar(edges(1:end-1)+.5*edgediff, hist2, .5, 'b', 'EdgeColor', 'none');
-    xlabel('PK slope');
+    bar(edges(1:end-1), hist1, .5, 'b', 'EdgeColor', 'none');
+    bar(edges(1:end-1)+.5*edgediff, hist2, .5, 'r', 'EdgeColor', 'none');
+    xlabel(['PK slope (' xlab ')']);
     ylabel('bootstrap histogram');
     set(gca, 'YTick', 0);
-    xlim([-.2, .2]);
+    xlim(range);
     
     if s_idx == 1
         [~, label1] = getStairVar(phases(1));
@@ -103,5 +125,30 @@ for s_idx=1:length(subjectIds)
         legend(label1, label2);
     end
 end
+
+%% Histogram ALL the subjects
+figure;
+hold on;
+binedges = linspace(range(1), range(2), 120);
+binwidth = (binedges(2) - binedges(1));
+bincenters = binedges(1:end-1) + binwidth / 2;
+counts = histcounts(all_slopes_diff(:), binedges);
+% --- Each subject as a different color ---
+% per_counts = zeros(length(subjectIds), length(bincenters));
+% for s_idx=1:length(subjectIds)
+%     per_counts(s_idx, :) = histcounts(all_slopes_diff(s_idx, :), binedges);
+% end
+% bar(bincenters, per_counts', 'stacked');
+% % --- All subjects plus ksdensity over medians ---
+% bar(bincenters, counts / sum(counts) / binwidth); % All subjects x all bootstraps combined
+% [density, dens_pts] = ksdensity(all_slopes_diff_medians);
+% plot(dens_pts, density, '-r', 'LineWidth', 2);
+% --- KS density of each subject as a different color ---
+for s_idx=1:length(subjectIds)
+    [density, dens_pts] = ksdensity(all_slopes_diff(s_idx, :));
+    plot(dens_pts, density, 'LineWidth', 2);
+end
+xlabel(['\Delta PK Slope (' xlab ')']);
+xlim(range);
 
 end
