2f5f95da65fffd47399b183e5271f6d49fe2ebb9
wrongu <rlange@ur.rochester.edu>
2018-06-28 14:04:52 -0400

    CustomRegression.expFit changes: (1) returns errors from inverse hessian, and (2) weak zero-mean prior on beta, which is otherwise unconstrained when scale is zero

--- +CustomRegression/expFit.m
+++ +CustomRegression/expFit.m
@@ -1,4 +1,4 @@
-function expfit = expFit(weights, errors)
+function [expfit, errors] = expFit(weights, errors)
 if nargin < 2
     variances = ones(size(weights));
 else
@@ -10,20 +10,30 @@ end
 p0 = [mean(weights) 0];
 
 options = optimoptions(@fmincon, 'SpecifyObjectiveGradient', true);
-expfit = fmincon(@(p) errfn(p, weights(:), variances(:)), p0, [], [], [], [], [0 -10], [inf 10], [], options);
+[expfit, ~, ~, ~, ~, ~, hessian] = fmincon(@(p) errfn(p, weights(:), variances(:)), ...
+    p0, [], [], [], [], [0 -10], [inf 10], [], options);
+errors = sqrt(diag(abs(inv(-hessian))));
 
 end
 
 function [err, grad] = errfn(p, w, v)
 scale = p(1);
 beta = p(2);
+betaPriorVar = 100;
 frames = (0:length(w)-1)';
 expvals = scale*exp(frames*beta);
-err = sum((w - expvals).^2 ./ v);
+err = sum((w - expvals).^2 ./ v) + beta^2 / betaPriorVar;
 
 if nargout >= 2
     gradScale = 2 * sum((scale*exp(2*frames*beta) - w.*exp(frames*beta)) ./ v);
-    gradBeta = 2 * sum(frames .* (expvals .* (expvals - w)) ./ v);
+    gradBeta = 2 * sum(frames .* (expvals .* (expvals - w)) ./ v) + 2 * beta / betaPriorVar;
     grad = [gradScale gradBeta];
+    
+    % if nargout >= 3
+    %     hessScale = 2 * sum(exp(2*frames*beta) ./ v);
+    %     hessScaleBeta = 2 * sum((2*scale*exp(2*frames*beta) - w.*exp(frames*beta)) .* frames ./ v);
+    %     hessBeta = 2 * sum((frames.^2 .* expvals .* (w + 2)) ./ v);
+    %     hess = [hessScale hessScaleBeta; hessScaleBeta hessBeta];
+    % end
 end
 end
