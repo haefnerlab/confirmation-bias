eadf4f5c4f0f0a3ec6930b5185434b577f738e6d
wrongu <rlange@ur.rochester.edu>
2018-07-27 09:53:40 -0400

    ViolinPlot subdirectory; minor change to startup

--- startup.m
+++ startup.m
@@ -2,6 +2,7 @@ if exist('psignifit', 'file')~=2, addpath('psignifit-master'); end
 if exist('smoothn', 'file')~=2, addpath('smoothn'); end
 if exist('fadecolors', 'file')~=2, addpath('tools'); end
 if exist('advancedcolormap', 'file')~=2, addpath('AdvancedColorMap'); end
+if exist('violinplot', 'file')~=2, addpath('Violinplot-Matlab'); end
 if exist('boundedline', 'file')~=2
     D = dir(fullfile('tools', 'boundedline-pkg'));
     for i=1:length(D)
@@ -13,8 +14,8 @@ end
 
 %% Create useful workspace variables for analysis
 
-ratioSubjects = arrayfun(@(i) sprintf('bpgFinaltest-subject%02d', i), setdiff(1:15, [1 12]), 'UniformOutput', false);
-noiseSubjects = arrayfun(@(i) sprintf('bpgFinaltest-subject%02d', i), setdiff(1:15, [1]), 'UniformOutput', false);
+ratioSubjects = arrayfun(@(i) sprintf('bpgFinaltest-subject%02d', i), setdiff(1:15, [1 5 12]), 'UniformOutput', false);
+noiseSubjects = arrayfun(@(i) sprintf('bpgFinaltest-subject%02d', i), setdiff(1:15, [1 5]), 'UniformOutput', false);
 
 % informed is the opposite of naive
 informedSubjects = arrayfun(@(i) sprintf('bpgFinaltest-subject%02d', i), [7 8 9], 'UniformOutput', false);
@@ -26,4 +27,4 @@ naiveBothSubjects = intersect(naiveRatioSubjects, naiveNoiseSubjects);
 % Re-order so that informed subjects are at the end
 ratioSubjects = [naiveRatioSubjects informedSubjects];
 noiseSubjects = [naiveNoiseSubjects informedSubjects];
-bothSubjects = intersect(ratioSubjects, noiseSubjects);
+bothSubjects = [intersect(naiveRatioSubjects, naiveNoiseSubjects) informedSubjects];
