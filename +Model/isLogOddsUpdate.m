73cff3a1c1f633ecf10c6ec07d568d18d8d90316
wrongu <rlange@ur.rochester.edu>
2018-10-02 12:40:58 -0400

    Sampling model vectorized. Benchmark: 10000 trials, 5 update, 5 samples. 15s down to 0.5s

--- +Model/isLogOddsUpdate.m
+++ +Model/isLogOddsUpdate.m
@@ -2,6 +2,7 @@ function [lpo, x_samples, weights] = isLogOddsUpdate(params, e, lpo)
 %MODEL.ISLOGODDSUPDATE compute update to log odds of C using importance sampling model.
 
 trials = size(e, 1);
+oz = ones(trials, 1);
 
 updates = params.updates;
 noise = params.noise;
@@ -22,32 +23,37 @@ for n=1:updates
     % Convert from lpo (log odds) to the probability that C=+1
     pC = 1 ./ (1 + exp(-lpo));
     
-    parfor t=1:trials
-        % Likelihood is a unimodal Gaussian centered on e(t), but it is convenient to create a
-        % mixture-of-Gaussians object to represent it.
-        likelihood = mog.create(e(t), sig_s, 1);
+    % Create likelihoods. Format is a matrix where each row specifies triples of [mu, sigma, pi] of
+    % a mixture of Gaussians. Only one mode in the likelihood, but it's useful to use the MoG
+    % format. See @mog.create
+    likelihoods = [e(:) sig_s*oz oz];
+    
+    % Create the prior on x by marginalizing over the current posterior of C. The prior is also a
+    % mixture of gaussians, but with 2 modes corresponding to C = +/-1
+    p = p_match * pC + (1 - p_match) * (1 - pC);
+    priors = [+oz, sig_x*oz, p, -oz, sig_x*oz, 1-p];
         
-        % Create the prior on x by marginalizing over the current posterior of C
-        p = p_match * pC(t) + (1 - p_match) * (1 - pC(t));
-        prior = mog.create([+1 -1], [sig_x sig_x], [p 1-p]);
-        
-        % Q is the distribution from which samples of x are drawn; it is the current estimate of the
-        % posterior over x using lpo as the prior over C
-        Q = mog.prod(likelihood, prior);
-        
-        % Draw samples from Q
-        x_samples(t, :, n) = mog.sample(Q, samples);
-        
-        % Get unnormalized importance weights for each sample
-        weights(t, :, n) = 1 ./ mog.pdf(x_samples(t, :, n), prior);
+    % Q is the distribution from which samples of x are drawn; it is the current estimate of the
+    % posterior over x using lpo as the prior over C
+    Q = mog.prod(likelihoods, priors);
+    
+    % Draw samples from Q
+    samp = mog.sample(Q, samples);
+    x_samples(:, :, n) = samp;
+    flat_samples = samp(:);
+    
+    % Get unnormalized importance weights for each sample (note that this is vectorized over trials,
+    % but we have to loop over samples. Typically number of samples << number of trials)
+    for s=1:samples
+        weights(:, s, n) = 1 ./ mog.pdf(x_samples(:, s, n), priors);
     end
 
     % Normalize importance-sampling weights
     weights(:, :, n) = weights(:, :, n) ./ sum(weights(:, :, n), 2);
 
     % Compute p(x|C=+1) and p(x|C=-1), then take weighted sum for each trial.
-    pCp = sum(mog.pdf(x_samples(:, :, n), p_x_Cp) .* weights(:, :, n), 2);
-    pCm = sum(mog.pdf(x_samples(:, :, n), p_x_Cm) .* weights(:, :, n), 2);
+    pCp = sum(reshape(mog.pdf(flat_samples, p_x_Cp), [trials samples]) .* weights(:, :, n), 2);
+    pCm = sum(reshape(mog.pdf(flat_samples, p_x_Cm), [trials samples]) .* weights(:, :, n), 2);
 
     % Log likelihood odds is log(pCp/pCm)
     llo = log(pCp) - log(pCm);
--- +Model/logLikelihoodOdds.m
+++ +Model/logLikelihoodOdds.m
@@ -5,6 +5,7 @@ netSig = sqrt(params.var_x + params.var_s);
 probPos = mog.create([+1, -1], [netSig, netSig], [params.p_match, 1-params.p_match]);
 probNeg = mog.create([+1, -1], [netSig, netSig], [1-params.p_match, params.p_match]);
 
-llo = mog.logpdf(data, probPos) - mog.logpdf(data, probNeg);
+llo = mog.logpdf(data(:), probPos) - mog.logpdf(data(:), probNeg);
+llo = reshape(llo, size(data));
 
 end
