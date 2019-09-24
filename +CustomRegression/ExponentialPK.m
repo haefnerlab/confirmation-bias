3b21a360a50f6be1c74b95a1ff70d247a9a6ab17
wrongu <rlange@ur.rochester.edu>
2019-09-24 10:00:40 -0400

    ExponentialPK: stabler initialization + fitting with prior; faster convergence with gradient

--- +CustomRegression/ExponentialPK.m
+++ +CustomRegression/ExponentialPK.m
@@ -1,7 +1,7 @@
 function [abb, postVal, errors] = ExponentialPK(data, responses, standardize)
 %EXPONENTIALPK Regress PK as an exponential function of time.
 %
-% abb = LinearPK(data, responses) returns abb = [alpha beta bias] such that w=alpha*exp(beta*f)
+% abb = EXPONENTIALPK(data, responses) returns abb = [alpha beta bias] such that w=alpha*exp(beta*f)
 % and choices~sigmoid(signal'*w+bias)
 
 if nargin < 3, standardize = 0; end
@@ -25,34 +25,44 @@ responses = 1.0 * responses(:);
 
 [~, frames] = size(data);
 
-    function NLL = neg_bernoulli_log_likelihood(abb)
+    function [NLP, gradNLP] = neg_log_posterior(abb)
         weights = abb(1) * exp(abb(2) * (0:frames-1));
         logits = data * weights(:) + abb(3);
         neg_log_bernoulli = -responses .* logits(:) + log(1 + exp(logits(:)));
-        NLL = sum(neg_log_bernoulli);
-        NLL = NLL + 1/2*abb(2)^2/100;
+        NLP = sum(neg_log_bernoulli);
+        % Add mild priors to alpha and beta terms for stability
+        neg_log_prior_beta = 1/2*abb(2)^2/10;
+        neg_log_prior_alpha = abs(abb(1))/10;
+        NLP = NLP + neg_log_prior_alpha + neg_log_prior_beta;
+        
+        if nargout >= 2
+            dNLP_dlogits = -responses + exp(logits(:))./(1+exp(logits(:)));
+            dlogits_dw = data';
+            dw_dalpha = exp(abb(2)*(0:frames-1));
+            dw_dbeta = abb(1)*(0:frames-1).*exp(abb(2)*(0:frames-1));
+            gradNLP = [dw_dalpha * dlogits_dw * dNLP_dlogits + 1/10, ...
+                dw_dbeta * dlogits_dw * dNLP_dlogits + abb(2)/10, ...
+                sum(dNLP_dlogits)];
+        end
     end
 
 compute_error = nargout > 2;
 
-glm_weights = glmfit(data, responses, 'binomial');
-ab = CustomRegression.expFit(glm_weights(2:end));
-init_guess = [ab glm_weights(1)];
-
-% Attenuate initial guess if it is extreme, simply by decaying parameter values towards zero (which
-% is, in this model, a lack of prediction)
-while abs(neg_bernoulli_log_likelihood(init_guess)) > 1e6
-    init_guess = init_guess / 2;
-end
+init_guess = [1 0 0];
 
 % Fit weights using 'fminunc', only computing the hessian (which is slow) if errors are requested.
+options = optimoptions('fminunc', ...
+    'Display', 'none', ...
+    'Algorithm', 'trust-region', ...
+    'SpecifyObjectiveGradient', true);
+
 if compute_error
-    [abb, negPostVal, ~, ~, ~, hessian] = fminunc(@neg_bernoulli_log_likelihood, init_guess);
+    [abb, negPostVal, ~, ~, ~, hessian] = fminunc(@neg_log_posterior, init_guess, options);
     % attempt to invert the hessian for standard error estimate - this sometimes fails silently,
     % returning NaN.
     errors = sqrt(diag(abs(inv(-hessian))));
 else
-    [abb, negPostVal] = fminunc(@neg_bernoulli_log_likelihood, init_guess);
+    [abb, negPostVal] = fminunc(@neg_log_posterior, init_guess, options);
 end
 postVal = -negPostVal;
 end
