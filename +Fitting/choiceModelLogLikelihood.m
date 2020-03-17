0da7316fc57ee3e55666b477f6d6a37e6faa14c8
wrongu <rlange@ur.rochester.edu>
2020-03-17 09:01:09 -0400

    Fitting boilerplate udpdates: added temperature, noise, and bound. choiceModelLogLH compatibility.

--- +Fitting/choiceModelLogLikelihood.m
+++ +Fitting/choiceModelLogLikelihood.m
@@ -12,21 +12,23 @@ likelihood_samples = cell(size(params));
 log_likelihood_per_trial = cell(size(params));
 log_likelihood = 0;
 
+if all(cellfun(@isempty, signals)), return; end
+
 for iSet=1:length(params)
-    params(iSet) = Fitting.sanitize(params(iSet));
+    thisParams = Fitting.sanitize(params(iSet));
     
-    params(iSet).var_s = params(iSet).var_s_per_sample * params(iSet).samples;
+    thisParams.var_s = thisParams.var_s_per_sample * thisParams.samples;
     
     % TODO - smarter setting of seed?
-    params(iSet).seed = randi(1000000000);
+    thisParams.seed = randi(1000000000);
     
     nTrials = length(choices{iSet});
     
     % Repeat signals to vectorize 'nInner' as if it were more trials
     signals{iSet} = repmat(signals{iSet}, nInner, 1);
     
-    sim_results = Model.runVectorized(params(iSet), signals{iSet});
-    prob_choice = 1 ./ (1 + exp(-sim_results.lpo(:, end)));
+    sim_results = Model.runVectorized(thisParams, signals{iSet});
+    prob_choice = 1 ./ (1 + exp(-sim_results.lpo(:, end) / thisParams.temperature));
     
     % Reshape prob_choice from model to [nTrials x nInner]
     prob_choice = reshape(prob_choice, nTrials, nInner);
@@ -35,8 +37,8 @@ for iSet=1:length(params)
     % the model on each choice.
     chosePos = choices{iSet} == +1;
     choseNeg = ~chosePos;
-    likelihood_samples{iSet}(chosePos, :) = params(iSet).lapse/2 + (1-params(iSet).lapse) * prob_choice(chosePos, :);
-    likelihood_samples{iSet}(choseNeg, :) = params(iSet).lapse/2 + (1-params(iSet).lapse) * (1-prob_choice(choseNeg, :));
+    likelihood_samples{iSet}(chosePos, :) = thisParams.lapse/2 + (1-thisParams.lapse) * prob_choice(chosePos, :);
+    likelihood_samples{iSet}(choseNeg, :) = thisParams.lapse/2 + (1-thisParams.lapse) * (1-prob_choice(choseNeg, :));
     
     log_likelihood_per_trial{iSet} = log(nanmean(likelihood_samples{iSet}, 2));
     log_likelihood = log_likelihood + nanmean(log_likelihood_per_trial{iSet}) * nTrials;
--- +Fitting/sanitize.m
+++ +Fitting/sanitize.m
@@ -1,18 +1,32 @@
 function params = sanitize(params)
 
+% Convert any field name that begins with 'log_' to exp(value)
+fields = fieldnames(params);
+for iF=1:length(fields)
+    if startsWith(fields{iF}, 'log_')
+        params.(fields{iF}(5:end)) = exp(params.(fields{iF}));
+    end
+end
+
 params.prior_C = clip(params.prior_C, 0, 1);
-params.lapse = clip(params.lapse, 0, 1);
-params.gamma = clip(params.gamma, 0, 1);
+params.lapse = clip(params.lapse, 1e-9, 1-1e-9);
+params.noise = clip(params.noise, 1e-9, inf);
+params.temperature = clip(params.temperature, 1e-9, inf);
+% Optimization may or may not choose to bound gamma in [0 1]. We *always* enforce [-1 1]
+params.gamma = clip(params.gamma, -1, 1);
+% During fitting of IS model, var_s and samples are highly correlated, so we reparameterize var_s as
+% variance per sample.
 if isfield(params, 'var_s_per_sample')
-    params.var_s_per_sample = clip(params.var_s_per_sample, 0, inf);
+    params.var_s_per_sample = clip(params.var_s_per_sample, 1e-9, inf);
     params.var_s = params.var_s_per_sample * params.samples;
 else
-    params.var_s = clip(params.var_s, 0, inf);
+    params.var_s = clip(params.var_s, 1e-9, inf);
     params.var_s_per_sample = params.var_s / params.samples;
 end
-params.var_x = clip(params.var_x, 0, inf);
+params.var_x = clip(params.var_x, 1e-9, inf);
 params.updates = clip(round(params.updates), 1, inf);
 params.samples = clip(round(params.samples), 1, inf);
+params.bound = clip(params.bound, 1e-9, inf);
 
 end
 
