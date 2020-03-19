30984fd37e43fd285871cf6328ff8401389d8861
wrongu <rlange@ur.rochester.edu>
2020-03-19 10:57:50 -0400

    Big Fitting.choiceModelLogProb update: using unbiased IBS log likelihood - no more inner loop!

--- +Fitting/choiceModelLogProb.m
+++ +Fitting/choiceModelLogProb.m
@@ -1,4 +1,17 @@
-function [log_post, log_prior, log_lh_per_trial, likelihood_samples] = choiceModelLogProb(params, prior_info, signals, choices, nInner)
+function [log_post, log_prior, log_lh_per_trial, est_variance] = choiceModelLogProb(params, prior_info, signals, choices, maxK)
+% FITTING.CHOICEMODELLOGPROB estimate log posterior probability of given params struct. Prior is
+% based on prior_info struct (see @Fitting.defaultDistributions). If prior_info is empty, prior is
+% not counted and only the log likelihood is computed. The log likelihood is stochastic but unbiased,
+% computed using the Inverse Binomial Sampling method of [1], clipped at a maximum of maxK
+% iterations.
+%
+% Optionally, params may be a struct array of params, and both 'signals' and 'choices' may be cell
+% arrays of the same size. The likelihood is then the product of likelihoods for each 'set' of data.
+%
+% [1] van Opheusden, B., Acerbi, L., & Ma, W. J. (2020). Unbiased and Efficient Log-Likelihood
+%   Estimation with Inverse Binomial Sampling. http://arxiv.org/abs/2001.03985
+
+if nargin < 5, maxK = 1e4; end
 
 if ~iscell(signals)
     signals = {signals};
@@ -8,7 +21,6 @@ end
 assert(length(params) == length(signals));
 assert(length(params) == length(choices));
 
-likelihood_samples = cell(size(params));
 log_lh_per_trial = cell(size(params));
 
 if all(cellfun(@isempty, signals)), return; end
@@ -26,37 +38,59 @@ for iF=1:length(prior_fields)
     end
 end
 
-%% Evaluate likelihood
+%% Evaluate likelihood using Inverse Binomial Sampling
+
+% Maximize pseudo-randomness inside the simulations
+randstate = rng();
+rng('shuffle');
+
 log_lh = 0;
-for iSet=1:length(params)
+for iSet=length(params):-1:1
     thisParams = Fitting.sanitize(params(iSet));
-        
-    % TODO - smarter setting of seed?
-    thisParams.seed = randi(1000000000);
     
     nTrials = length(choices{iSet});
+    assert(all(ismember(choices{iSet}, [-1 +1])));
+
+    % All we need for the IBS likelihood is the number of model simulations, each trial, until we
+    % match the choice on that trial
+    nSamplesToMatch = maxK * ones(nTrials, 1);
+
+    % Only keep re-running simulations on trials that haven't yet been matched
+    unmatched = 1:nTrials;
     
-    % Repeat signals to vectorize 'nInner' as if it were more trials
-    signals{iSet} = repmat(signals{iSet}, nInner, 1);
+    % Loop forever (up to maxK) until no unmatched trials remain
+    k = 1;
+    while ~isempty(unmatched) && k < maxK
+        % Run the model on unmatched trials
+        sim_results = Model.runVectorized(thisParams, signals{iSet}(unmatched, :));
+
+        % For all that now match... record 'k' and remove those trials
+        matching_subset = sim_results.choices == choices{iSet}(unmatched);
+        nSamplesToMatch(unmatched(matching_subset)) = k;
+        unmatched(matching_subset) = [];
+
+        % Advance...
+        k = k+1;
+    end
+
+    % See eq. 14 in reference [1]; psi(0,x) is Matlab's builtin digamma function of x, i.e. the
+    % first derivative of log(gamma(x))
+    log_lh_per_trial = psi(0,1) - psi(0,nSamplesToMatch);
+    log_lh(iSet) = sum(log_lh_per_trial);
     
-    sim_results = Model.runVectorized(thisParams, signals{iSet});
-    prob_choice = 1 ./ (1 + exp(-sim_results.lpo(:, end) / thisParams.temperature));
-    
-    % Reshape prob_choice from model to [nTrials x nInner]
-    prob_choice = reshape(prob_choice, nTrials, nInner);
-    
-    % likelihood_samples is [nTrials x nInner] and contains the full choice likelihood for each run of
-    % the model on each choice.
-    chosePos = choices{iSet} == +1;
-    choseNeg = ~chosePos;
-    likelihood_samples{iSet}(chosePos, :) = thisParams.lapse/2 + (1-thisParams.lapse) * prob_choice(chosePos, :);
-    likelihood_samples{iSet}(choseNeg, :) = thisParams.lapse/2 + (1-thisParams.lapse) * (1-prob_choice(choseNeg, :));
-    
-    log_lh_per_trial{iSet} = log(nanmean(likelihood_samples{iSet}, 2));
-    log_lh = log_lh + sum(log_lh_per_trial{iSet});
+    % See eq. 16 in reference [1]; psi(1,x) is the trigamma function of x, i.e. the second
+    % derivative of log(gamma(x))
+    var_ll_per_trial{iSet} = psi(1,1) - psi(1,nSamplesToMatch);
 end
 
-%% Combine prior and likelihood
-log_post = log_prior + log_lh;
+rng(randstate);
 
+%% Combine prior and likelihood
+
+% Unbiased estimate of the log posterior...
+log_post = log_prior + sum(log_lh);
+
+% Estimate of the variance in log_post
+all_ll_var = vertcat(var_ll_per_trial{:});
+est_variance = sum(all_ll_var);
 end
