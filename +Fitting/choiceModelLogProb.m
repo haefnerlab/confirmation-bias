f2bb3ef4061f4c40b0fbdf6eb7ae110821677d4e
wrongu <rlange@ur.rochester.edu>
2020-03-21 08:38:22 -0400

    Fitting.choiceModelLogProb: replaced maxK break condition with lower bound on L as per IBS authors' recommendation

--- +Fitting/choiceModelLogProb.m
+++ +Fitting/choiceModelLogProb.m
@@ -1,4 +1,4 @@
-function [log_post, log_prior, log_lh_per_trial, est_variance] = choiceModelLogProb(params, prior_info, signals, choices, maxK)
+function [log_post, log_like, est_variance, lower_bound] = choiceModelLogProb(params, prior_info, signals, choices, lower_bound)
 % FITTING.CHOICEMODELLOGPROB estimate log posterior probability of given params struct. Prior is
 % based on prior_info struct (see @Fitting.defaultDistributions). If prior_info is empty, prior is
 % not counted and only the log likelihood is computed. The log likelihood is stochastic but unbiased,
@@ -11,19 +11,38 @@ function [log_post, log_prior, log_lh_per_trial, est_variance] = choiceModelLogP
 % [1] van Opheusden, B., Acerbi, L., & Ma, W. J. (2020). Unbiased and Efficient Log-Likelihood
 %   Estimation with Inverse Binomial Sampling. http://arxiv.org/abs/2001.03985
 
-if nargin < 5, maxK = 1e4; end
-
 if ~iscell(signals)
     signals = {signals};
     choices = {choices};
 end
 
-assert(length(params) == length(signals));
-assert(length(params) == length(choices));
-
-log_lh_per_trial = cell(size(params));
+nSets = length(params);
+assert(nSets == length(signals));
+assert(nSets == length(choices));
 
 if all(cellfun(@isempty, signals)), return; end
+
+nTotalTrials = sum(cellfun(@length, choices));
+if nargin < 5
+    % By default, the lower bound assumes a uniform distribution over the two choices each trial.
+    lower_bound = -nTotalTrials * log(2);
+end
+
+% Sanity check that all choices are in [-1 +1]
+for iSet=1:nSets
+    if ~all(ismember(choices{iSet}, [-1 +1]))
+        error('Choices in wrong space! Should be in [-1 +1] but seeing %s.', mat2str(unique(choices{iSet})));
+    end
+end
+
+% Out of the indices in 1:nTotalTrials, they are split into 'sets', one per triplet of
+% (param,signal,choice). 'setIdx' is the logical mask of which trials belong to which set.
+setIdx = false(nTotalTrials, nSets);
+iNextStart = 1;
+for iSet=1:nSets
+    setIdx(iNextStart:iNextStart+length(choices{iSet})-1, iSet) = true;
+    iNextStart = iNextStart+length(choices{iSet});
+end
 
 %% Evaluate prior
 log_prior = 0;
@@ -44,49 +63,66 @@ end
 randstate = rng();
 rng('shuffle');
 
-log_lh = 0;
-for iSet=length(params):-1:1
-    nTrials = length(choices{iSet});
-    assert(all(ismember(choices{iSet}, [-1 +1])));
+% All we need for the IBS likelihood is the number of model simulations, each trial, until we
+% match the choice on that trial
+nSamplesToMatch = ones(nTotalTrials, 1);
 
-    % All we need for the IBS likelihood is the number of model simulations, each trial, until we
-    % match the choice on that trial
-    nSamplesToMatch = maxK * ones(nTrials, 1);
+% Only keep re-running simulations on trials that haven't yet been matched
+matched = false(nTotalTrials, 1);
 
-    % Only keep re-running simulations on trials that haven't yet been matched
-    unmatched = 1:nTrials;
-    
-    % Loop forever (up to maxK) until no unmatched trials remain
-    k = 1;
-    while ~isempty(unmatched) && k < maxK
-        % Run the model on unmatched trials
-        sim_results = Model.runVectorized(params(iSet), signals{iSet}(unmatched, :));
+% At each iteration, the upper bound is based on L for all 'matched' trials plus what it would be
+% for the 'unmatched' trials if they all completed on the next iteration.
+L_hat_upper_bound = 0;
 
-        % For all that now match... record 'k' and remove those trials
-        matching_subset = sim_results.choices == choices{iSet}(unmatched);
-        nSamplesToMatch(unmatched(matching_subset)) = k;
-        unmatched(matching_subset) = [];
-
-        % Advance...
-        k = k+1;
+% Loop forever (up to maxK) until no unmatched trials remain
+k = 1;
+while ~all(matched) && L_hat_upper_bound > lower_bound
+    for iSet=length(params):-1:1
+        if all(matched(setIdx(:,iSet))), continue; end
+        
+        % Run the model only on unmatched trials within this set
+        sim_results = Model.runVectorized(params(iSet), signals{iSet}(~matched & setIdx(:,iSet), :));
+        
+        % For all that now match... record 'k' and mark those trials as complete
+        matching_subset = sim_results.choices == choices{iSet}(~matched & setIdx(:,iSet));
+        if any(matching_subset)
+            % Work out the indices out of 1:nTotalTrials that we just matched
+            sim_idxs = find(~matched & setIdx(:,iSet));
+            match_idxs = sim_idxs(matching_subset);
+            % Mark those trials as completed
+            matched(match_idxs) = true;
+            % Store their 'k'
+            nSamplesToMatch(match_idxs) = k;
+        end
     end
-
+        
+    % Upper-bound on L-hat is the estimate of L under the assumption that all other trials will be
+    % matched on the next step.
+    nSamplesToMatch(~matched) = k+1;
+    
     % See eq. 14 in reference [1]; psi(0,x) is Matlab's builtin digamma function of x, i.e. the
     % first derivative of log(gamma(x))
-    log_lh_per_trial = psi(0,1) - psi(0,nSamplesToMatch);
-    log_lh(iSet) = sum(log_lh_per_trial);
-    
-    % See eq. 16 in reference [1]; psi(1,x) is the trigamma function of x, i.e. the second
-    % derivative of log(gamma(x))
-    var_ll_per_trial{iSet} = psi(1,1) - psi(1,nSamplesToMatch);
+    L_hat_upper_bound = sum(psi(0,1) - psi(0,nSamplesToMatch));
+
+    % Advance...
+    k = k+1;
 end
+    
+% See eq. 14 in reference [1]; psi(0,x) is Matlab's builtin digamma function of x, i.e. the
+% first derivative of log(gamma(x))
+log_lh_per_trial = psi(0,1) - psi(0,nSamplesToMatch);
+
+% See eq. 16 in reference [1]; psi(1,x) is the trigamma function of x, i.e. the second
+% derivative of log(gamma(x))
+var_ll_per_trial{iSet} = psi(1,1) - psi(1,nSamplesToMatch);
 
 rng(randstate);
 
 %% Combine prior and likelihood
 
 % Unbiased estimate of the log posterior...
-log_post = log_prior + sum(log_lh);
+log_like = sum(log_lh_per_trial);
+log_post = log_prior + log_like;
 
 % Estimate of the variance in log_post
 all_ll_var = vertcat(var_ll_per_trial{:});
