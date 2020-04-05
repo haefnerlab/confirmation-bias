b476fea26aed74061919b556f2ad5dfb9c60d19d
wrongu <rlange@ur.rochester.edu>
2020-04-05 11:13:55 -0400

    +fitting/sampleModelMH : call mymhsample to store scores; also check whether model is deterministic or stochastic

--- +Fitting/sampleModelMH.m
+++ +Fitting/sampleModelMH.m
@@ -1,6 +1,6 @@
-function [samples, accept, params_samples] = fitChoicesMH(signals, choices, base_params, n_samples, distributions, n_inner, sample_burnin, sample_thin, chkpt)
-if nargin < 6 || isempty(n_inner), n_inner = 50; end
-if nargin < 7 || isempty(sample_burnin), sample_burnin = 500; end
+function [samples, accept, smpl_info] = sampleModelMH(signals, choices, base_params, n_samples, distributions, ibs_repeats, sample_burnin, sample_thin, chkpt)
+if nargin < 6 || isempty(ibs_repeats), ibs_repeats = 5; end
+if nargin < 7 || isempty(sample_burnin), sample_burnin = 100; end
 if nargin < 8 || isempty(sample_thin), sample_thin = 10; end
 if nargin < 9, chkpt = ''; end
 
@@ -12,9 +12,22 @@ for iField=1:length(fields)
     end
 end
 
-% Wrap Fitting.choiceModelLogProb to make log-posterior function
-log_post = @(smpl, fields, distribs) Fitting.choiceModelLogProb(Fitting.setParamsFields(base_params, fields, smpl), ...
-    distribs, signals, choices, n_inner);
+% If ibs_repeats is set to 0, it indicates that we should use the deterministic/exact likelihood
+% from @Fitting.choiceModelLogProb (which is biased if stochastic) rather than the stochastic but
+% unbiased estimator @Fitting.choiceModelLogProbIBS
+if ibs_repeats == 0
+    if Model.isStochastic(base_params)
+        warning('IBS is recommended for stochastic models! Set ibs_repeats > 0!');
+    end
+    log_post = @(smpl) Fitting.choiceModelLogProb(...
+        Fitting.setParamsFields(base_params, fields, smpl), distributions, signals, choices, 1);
+else
+    if ~Model.isStochastic(base_params)
+        warning('IBS is not recommended for deterministic models! Set ibs_repeats = 0!');
+    end
+    log_post = @(smpl) Fitting.choiceModelLogProbIBS(...
+        Fitting.setParamsFields(base_params, fields, smpl), distributions, signals, choices, [], ibs_repeats);
+end
 
     function x = proprnd(x, fields, distribs)
         for jField=1:length(fields)
@@ -47,6 +60,7 @@ if ~isempty(chkpt)
         ld = load(fullfile(pth, chkfiles(isrt(iChk)).name));
         samples{iChk} = ld.batch_sample;
         net_accept(iChk) = ld.batch_accept;
+        batch_smpl_info(iChck) = ld.batch_info;
     end
     start_batch = length(chkfiles)+1;
 else
@@ -59,9 +73,9 @@ if isempty(samples{1})
     disp('MHSample init');
     for iInit=100:-1:1
         init_smpl(iInit, :) = cellfun(@(f) distributions.(f).priorrnd(1), fields);
-        init_lp(iInit) = log_post(init_smpl(iInit, :), fields, distributions);
+        [init_lp(iInit), ~, init_var(iInit)] = log_post(init_smpl(iInit, :));
     end
-    [~,idx] = max(init_lp);
+    [~,idx] = max(init_lp+3*sqrt(init_var));
     init_smpl = init_smpl(idx, :);
 end
 
@@ -74,8 +88,8 @@ for iBatch=start_batch:n_batch
     end
     
     tstart = tic;
-    [batch_sample, batch_accept] = mhsample(init_smpl, batch_size, ...
-        'logpdf', @(x) log_post(x, fields, distributions), ...
+    [batch_sample, batch_accept, batch_info] = mymhsample(init_smpl, batch_size, ...
+        'logpdf', log_post, ...
         'proprnd', @(x) proprnd(x, fields, distributions), ...
         'logproppdf', @(x1, x2) logproppdf(x1, x2, fields, distributions), ...
         'burnin', sample_burnin, 'thin', sample_thin);
@@ -83,6 +97,7 @@ for iBatch=start_batch:n_batch
     
     samples{iBatch} = batch_sample;
     net_accept(iBatch) = batch_accept;
+    batch_smpl_info(iBatch) = batch_info;
 
     t_remain = (n_batch - iBatch) * nanmean(batch_time);
     fprintf('MH Sample :: Batch %03d of %03d\tAccept=%.1f%%\tETA=%.1fs\n', iBatch, n_batch, 100*nanmean(net_accept), t_remain);
@@ -90,17 +105,14 @@ for iBatch=start_batch:n_batch
     if ~isempty(chkpt)
         savename = [chkpt sprintf('-batch%04d.mat', iBatch)];
         disp(['Saving to ' savename]);
-        save(savename, 'batch_sample', 'batch_accept');
+        save(savename, 'batch_sample', 'batch_accept', 'batch_info');
     end
 end
 
 samples = vertcat(samples{:});
 accept = nanmean(net_accept);
-
-%% Assign results back to struct array
-
-for iSample=size(samples, 1):-1:1
-    params_samples(iSample) = Fitting.setParamsFields(base_params, fields, samples(iSample, :));
-end
+smpl_info.logpdf = vertcat(batch_smpl_info.logpdf);
+smpl_info.loglike = vertcat(batch_smpl_info.loglike);
+smpl_info.variance = vertcat(batch_smpl_info.variance);
 
 end
