0d3e6d38b12602f4313520fabea03e5aeabcb8a9
wrongu <rlange@ur.rochester.edu>
2020-04-09 09:41:47 -0400

    Massive simplification of Subject->Model conversion.. just get sub-threshold stimuli for each condition and call it a day

--- GetSubjectDataForFitting.m
+++ GetSubjectDataForFitting.m
@@ -1,89 +1,41 @@
-function [param_set, stim_set, choice_set, trial_set] = GetSubjectDataForFitting(SubjectData, kernel_kappa, signal_scale, base_params, datadir)
+function [param_set, stim_set, choice_set, trial_set] = GetSubjectDataForFitting(subjectId, kernel_kappa, base_params, datadir)
 
 if ~exist('datadir', 'var'), datadir=fullfile(pwd, '../RawData'); end
 memodir = fullfile(datadir, '../Precomputed');
 
 if ~exist('base_params', 'var') || isempty(base_params)
-    base_params = Model.newModelParams('model', 'is', 'frames', SubjectData.number_of_images, ...
-        'trials', length(SubjectData.choice), 'gamma', 0.1, 'samples', 5, 'updates', 5, 'var_x', 0.1);
+    % By default, use parameters from qualitative fit that reproduced PK trend
+    base_params = Model.newModelParams('model', 'is', 'gamma', 0.1, 'samples', 5, 'updates', 5, ...
+        'var_x', 0.1, 'lapse', 0.01, 'temperature', 0.05);
 end
 
-% Note: assuming 'true_ratio', 'noise' (aka kappa), and 'contrast' are the only parameters changing
+% Load subject data. '1' is 'ratio' phase and '2' is 'noise' phase
+SubjectDataRatio = LoadAllSubjectData(subjectId, 1, datadir);
+SubjectDataNoise = LoadAllSubjectData(subjectId, 2, datadir);
 
-unsigned_ratio = max(SubjectData.true_ratio, 1-SubjectData.true_ratio);
-allStimParameters = [SubjectData.noise(:) unsigned_ratio(:) SubjectData.contrast(:)];
-[unqStim, ~, idxBwd] = unique(allStimParameters, 'rows');
-nUnqStim = size(unqStim, 1);
+% Get signals from reconstructed stimuli
+stim_set{1} = LoadOrRun(@ComputeFrameSignals, {SubjectDataRatio, kernel_kappa}, ...
+    fullfile(memodir, ['perFrameSignals-' subjectId '-' num2str(kernel_kappa) '-' SubjectDataRatio.phase '.mat']));
+stim_set{2} = LoadOrRun(@ComputeFrameSignals, {SubjectDataNoise, kernel_kappa}, ...
+    fullfile(memodir, ['perFrameSignals-' subjectId '-' num2str(kernel_kappa) '-' SubjectDataNoise.phase '.mat']));
 
-stim_gen = struct('kappa', SubjectData.noise(1), 'stim_size', SubjectData.stim_size, 'frames', SubjectData.number_of_images, ...
-    'stim_sp_freq_cpp', SubjectData.stim_sp_freq_cpp, 'stim_std_sp_freq_cpp', SubjectData.stim_std_sp_freq_cpp, ...
-    'contrast', SubjectData.contrast(1), 'annulus', SubjectData.annulus);
+% Select sub-threshold trials
+[~, trial_set{1}] = GaborThresholdTrials(SubjectDataRatio, 1, .6, .4);
+[~, thresh] = GaborAnalysis.getThresholdWindow(SubjectDataNoise, 2, 0.5, 0.7, memodir);
+[~, trial_set{2}] = GaborThresholdTrials(SubjectDataNoise, 1, thresh);
 
-sigs = LoadOrRun(@ComputeFrameSignals, {SubjectData, kernel_kappa}, ...
-    fullfile(memodir, ['perFrameSignals-' SubjectData.subjectID '-' num2str(kernel_kappa) '-' SubjectData.phase '.mat']));
+% Restrict stim and choices to sub-threshold in each task
+stim_set{1} = stim_set{1}(trial_set{1}, :);
+stim_set{2} = stim_set{2}(trial_set{2}, :);
+choice_set{1} = SubjectDataRatio.choice(trial_set{1});
+choice_set{2} = SubjectDataNoise.choice(trial_set{2});
 
-for iStim=nUnqStim:-1:1
-    trials = all(allStimParameters == unqStim(iStim, :), 2);
-    
-    sig_sign = sign(SubjectData.frame_categories(trials, :));
-    
-    stim_gen.contrast = unqStim(iStim, 3);
-    stim_gen.kappa = unqStim(iStim, 1);
-    uid_parts = cellfun(@(f) [f '=' num2str(stim_gen.(f))], fieldnames(stim_gen), 'uniformoutput', false);
-    uid = ['bpgstats-' strjoin(uid_parts, '-') '-kernelk=' num2str(kernel_kappa)];
-    [mu_sig(iStim), var_sig(iStim), ~, ~] = LoadOrRun(@bpg.getSignalStats, {stim_gen, kernel_kappa}, ...
-        fullfile(memodir, [uid '.mat'])); 
-end
-
-% Get expected d' of signals at each set of stimulus parameters, accounting for internal noise
-dprime = mu_sig ./ sqrt(var_sig + sensor_noise);
-[sensory_info, model_var_s] = Model.dprimeToSI(dprime);
-
-%% Construct model params and group trial together per unique stimulus combination
-param_set  = repmat(base_params, nUnqStim, 1);
-stim_set   = cell(size(param_set));
-choice_set = cell(size(param_set));
-trial_set  = cell(size(param_set));
-
-for iStim=nUnqStim:-1:1
-    tr = idxBwd == iStim;
-
-    param_set(iStim).trials = sum(tr);
-    trial_set{iStim} = tr;
-    
-    % Record all choices the subject made on these trials, using [-1,+1] convention rather than [0,1].
-    choice_set{iStim} = sign(double(SubjectData.choice(tr)) - 0.5);
-    % By convention, the model expects a column vector of choices
-    choice_set{iStim} = choice_set{iStim}(:);
-
-    % Record effective sensory and category information for this set of trials
-    si = max(.51, min(.99, sensory_info(iStim)));
-    ci = max(.51, min(.99, unqStim(iStim, 2)));
-    param_set(iStim) = Model.setCategorySensoryInfo(param_set(iStim), ci, si);
-
-    % Add other Subject's stimulus parameters to the model params for debugging - has no effect on
-    % the model behavior
-    param_set(iStim).kappa = unqStim(iStim, 1);
-    param_set(iStim).contrast = unqStim(iStim, 3);
-
-    % Recording stimuli is less straightforward... whereas 's_hat' all have approximately the
-    % same variance per difficulty level with changing means, the model is expecting signals
-    % drawn from a gaussian with mean +/- 1 and variance that depends on the sensory info.
-    bin_sgn = sign(SubjectData.frame_categories(tr, :));
-    bin_unsgn_sigs = sigs(tr, :) .* bin_sgn;
-
-    % Z-score signals based on their empirical distribution.
-    bin_zscore_sigs = (bin_unsgn_sigs - mu_sig(iStim)) ./ var_sig(iStim);
-
-    % Convert back to magnitudes expected by the model, i.e. Gaussian centered at +1 with variance
-    % set by sensory info.
-    model_unsgn_sigs = 1 + bin_zscore_sigs.*sqrt(param_set(iStim).var_s);
-    stim_set{iStim} = model_unsgn_sigs.*bin_sgn;
-end
-
-nData = cellfun(@length, choice_set);
-if sum(nData(:)) ~= length(SubjectData.choice)
-    warning('%d of %d trials dropped in conversion process!', ...
-        length(SubjectData.choice) - sum(nData(:)), length(SubjectData.choice));
-end
+% Sub-threshold data in the 'ratio' condition (HSLC) has category information 0.6 since all 6:4 or
+% 4:6 trials are included in the analysis. Sub-threshold data in the 'noise' condition (LSHC) has
+% category info 0.9 since there was always a single 'pulsed' frame of the opposite category. SI is
+% likewise set to 0.6 and 0.9 in each condition, but this is essentially arbitrary since it has no
+% effect on the model's behavior for a given log-likelhiood odds; as long as the signal_scale
+% mapping from stim to model evidence is fit, the actual value of SI is irrelevant.
+param_set(1) = Model.setCategorySensoryInfo(base_params, .6, .9);
+param_set(2) = Model.setCategorySensoryInfo(base_params, .9, .6);
 end
