08dfc2b2a8958e469f2b326864af52f6caa4d198
wrongu <rlange@ur.rochester.edu>
2020-06-18 15:59:19 -0400

    ITBParameter plot calls chain-weighting routine (tools submodule update) + some plotting cosmetics

--- ITBParameterPlot.m
+++ ITBParameterPlot.m
@@ -6,41 +6,55 @@ if nargin < 6, memodir = fullfile(datadir, '..', 'Precomputed'); end
 
 % NOTE: doesn't work across phases w/ different fields!
 
+
 allsamples = zeros(0, length(plot_fields));
 allgroups = [];
 for iSub=1:length(subjectIds)
     for iPhz=1:length(phases)
-        [chains, fields, ~, ~, ~, ~, ~] = LoadOrRun(@GetITBPosteriorSamples, ...
+        [chains, fields{iPhz}, loglikes{iSub,iPhz}, ~, ~, ~, ~] = LoadOrRun(@GetITBPosteriorSamples, ...
             {subjectIds{iSub}, phases{iPhz}, 0, true, 1:12, datadir, memodir}, ...
             fullfile('tmp-mh', ['ITB-cache-' subjectIds{iSub} '-' phases{iPhz} '.mat']));
+
         samples = vertcat(chains{:});
-        
-        % Format expected by 'boxplot' below is a massive concatenation of all samples, but with a
-        % 'group' id per row indicating subject x condition.
-        groupId = (iSub-1)*length(phases)+iPhz;
-        allsamples(end+1:end+size(samples,1), 1:size(samples,2)) = samples;
-        allgroups = vertcat(allgroups, groupId*ones(size(samples,1),1));
-        groupname{groupId} = sprintf('%s [%s]', shortname(subjectIds{iSub}), phases{iPhz});
+        values{iSub,iPhz} = zeros(size(samples,1), length(plot_fields));
+        for iPara=length(plot_fields):-1:1
+            iF = cellfun(@(f) contains(f, plot_fields{iPara}, 'ignorecase', true), fields{iPhz});
+            if log_flag(iPara)
+                values{iSub,iPhz}(:,iPara) = log10(samples(:,iF));
+            else
+                values{iSub,iPhz}(:,iPara) = samples(:,iF);
+            end
+        end
     end
 end
 
-%% Pull out just 'plot_fields' columns (and maybe log transform)
+%% Get chain-reweighted moments and quantiles
+range_lo = min(vertcat(values{:}));
+range_hi = max(vertcat(values{:}));
 for iPara=length(plot_fields):-1:1
-    iF = cellfun(@(f) contains(f, plot_fields{iPara}, 'ignorecase', true), fields);
-    if log_flag(iPara)
-        allvalues(:,iPara) = log10(allsamples(:,iF));
-        plot_fields{iPara} = ['log_{10}(' plot_fields{iPara} ')'];
-    else
-        allvalues(:,iPara) = allsamples(:,iF);
-    end
+    edges(iPara, :) = linspace(range_lo(iPara), range_hi(iPara), 101);
 end
 
-range_lo = quantile(allvalues, .01);
-range_hi = quantile(allvalues, .99);
+for iSub=length(subjectIds):-1:1
+    for iPhz=length(phases):-1:1
+        [~, quantiles{iSub,iPhz}, pmfs{iSub,iPhz}] = ...
+            sampleQuantilesReweightChains(values{iSub,iPhz}, loglikes{iSub,iPhz}, [.025 .25 .5 .75 .975], edges);
+    end
+end
 
 %% Scatters
 scatter_fig = figure;
 for iPara=1:length(plot_fields)
+    subplot(1, length(plot_fields), iPara); hold on;
+    for iSub=1:length(subjectIds)
+        [m1, l1, u1] = deal(quantiles{iSub,1}(iPara,3), quantiles{iSub,1}(iPara,1), quantiles{iSub,1}(iPara,5));
+        [m2, l2, u2] = deal(quantiles{iSub,2}(iPara,3), quantiles{iSub,2}(iPara,1), quantiles{iSub,2}(iPara,5));
+        errorbar(m1, m2, m2-l2, u2-m2, m1-l1, u1-m1, '.b');
+    end
+    axis equal; axis square; grid on;
+    title(plot_fields{iPara});
+    xlabel(phases{1}); ylabel(phases{2});
+    uistack(plot(xlim, xlim, '-k', 'HandleVisibility', 'off'), 'bottom');
 end
 
 %% Marginal histograms figure
@@ -52,14 +66,9 @@ for iSub=1:length(subjectIds)
         subplot(length(subjectIds), length(plot_fields), (iSub-1)*length(plot_fields)+iPara);
         hold on;
         
-        bin_edges = linspace(range_lo(iPara), range_hi(iPara), 101);
-        for iPhz=1:length(phases)
-            % Format expected by 'boxplot' below is a massive concatenation of all samples, but with a
-            % 'group' id per row indicating subject x condition.
-            groupId = (iSub-1)*length(phases)+iPhz;
-            
-            histogram(allvalues(allgroups == groupId, iPara), bin_edges, 'EdgeAlpha', 0.1);
-        end
+        ctrs = (edges(iPara,1:end-1) + edges(iPara,2:end))/2;
+        bar(ctrs, pmfs{iSub, 1}(iPara, :), 1, 'FaceAlpha', .5, 'EdgeAlpha', 0);
+        bar(ctrs, pmfs{iSub, 2}(iPara, :), 1, 'FaceAlpha', .5, 'EdgeAlpha', 0);
         
         set(gca, 'YTick', 0);
         if iSub < length(subjectIds)
@@ -77,9 +86,27 @@ end
 box_fig = figure;
 for iPara=1:length(plot_fields)
     subplot(1, length(plot_fields), iPara); hold on;
-    boxplot(allvalues(:,iPara), allgroups, 'plotstyle', 'compact', 'symbol', '', 'colorgroup', mod(allgroups, length(phases)), 'orientation', 'horizontal');
+
+    % 95% CI as a thin line
+    for iSub=1:length(subjectIds)
+        plot(quantiles{iSub,1}(iPara,[1 5]), iSub*[1 1], '-', 'Color', 'r', 'LineWidth', 1);
+        plot(quantiles{iSub,2}(iPara,[1 5]), iSub*[1 1]+.25, '-', 'Color', 'b', 'LineWidth', 1);
+    end
+
+    % 50% CI as a thick line
+    for iSub=1:length(subjectIds)
+        plot(quantiles{iSub,1}(iPara,[2 4]), iSub*[1 1], '-', 'Color', 'r', 'LineWidth', 2);
+        plot(quantiles{iSub,2}(iPara,[2 4]), iSub*[1 1]+.25, '-', 'Color', 'b', 'LineWidth', 2);
+    end
+
+    % Median as a black dot
+    for iSub=1:length(subjectIds)
+        plot(quantiles{iSub,1}(iPara,3), iSub, '.', 'Color', 'k');
+        plot(quantiles{iSub,2}(iPara,3), iSub+.25, '.', 'Color', 'k');
+    end
+
     title(plot_fields{iPara});
-    set(gca, 'YTick', 1:length(groupname), 'YTickLabel', groupname);
+    set(gca, 'YTick', 1:length(subjectIds), 'YTickLabel', cellfun(@shortname, subjectIds, 'uniformoutput', false));
     grid on;
 end
 
